3991ec6f0a0094b53f1cd2773a83b2a895c7abaf
vanessa borges <vanessacamahuali28@gmail.com>
2022-03-16 10:18:01 -0300

    novo html,  css ,printando cards

--- src/data.js
+++ src/data.js
@@ -1,76 +1,42 @@
-//Funcão para filtrar por funções
-export const filterRole = (champions, roles) => {
-  const filterChampions = champions.filter(champion => champion.tags.includes(roles));
-  return filterChampions
-};
+export const filterByKey = (dataLOL, role) => {
+  const arrayFilter = dataLOL.filter(champion => champion.tags.includes(role));
+  return arrayFilter; 
+}
 
-export const orderAlpha = (champions => {
-  return champions.sort((a, b) => {
-     if (a.name > b.name) return -1;
-     else if (a.name < b.name) return -1;
-  });
+export const sortChampions = (dataLOL) => dataLOL.sort((item1, item2) => { 
+   return (item1.name > item2.name) ? -1 : 1
+})
+
+export const statistics1 = (dataLOL) => {
+  let arrayAttack= []; 
+dataLOL.forEach((player) => {
+  const championAttack= player.info.attack;
+ arrayAttack.push(championAttack);
+});
+  let sumAttack= arrayAttack.reduce((a, b) => a + b , 0);
+  let averageAttack= sumAttack/dataLOL.length;
+  return averageAttack;
+
+}
+
+export const statistics2= (dataLOL) => {
+  let arrayDamage= []; 
+dataLOL.forEach((player) => {
+  const championDamage= player.stats.attackdamage;
+  arrayDamage.push(championDamage);
 });
 
-//pucha a dificuldade de todos os campeões
-export const filterDifficulty = (champions, difficulty) => {
-   return champions.filter(campeao =>campeao.title === difficulty)
-   
-  /*
-  let emptyLow = [];
-  let emptyMed = [];
-  let emptyHig = [];
-  champions.forEach(champion => {
-     const getDifficulty = champion.info.difficulty;
-     if (getDifficulty <= 4) {
-        emptyLow.push(champion);
-     }
-     else if (getDifficulty > 4 && getDifficulty < 8) {
-        emptyMed.push(champion);
-     }
-     else if (getDifficulty >= 8 && getDifficulty <= 10) {
-        emptyHig.push(champion);
-     }
-  });
-  if (difficulty == 'Baixa') {
-     return emptyLow
-  } else if (difficulty == 'Média') {
-     return emptyMed
-  } else if (difficulty == 'Alta') {
-     return emptyHig
-  }
-  */
-};
-
-//pucha stats e mostra os campeões
-export const filterStats = (champions, stats) => {
-  let topDamage = [];
-  let topHealth = [];
-  let topMana = [];
-  champions.forEach(champion => {
-     const baseDamage = champion.stats.attackdamage
-     const growthDamage = champion.stats.attackdamageperlevel
-     const averageDamage = parseInt(baseDamage / growthDamage)
-     if (averageDamage > 25 || averageDamage === 40) {
-        topDamage.push(champion)
-     }
-     const baseHealth = champion.stats.hpregen
-     const growthHealth = champion.stats.hpregenperlevel
-     const averageHealth = parseInt(baseHealth / growthHealth)
-     if (averageHealth === 15) {
-        topHealth.push(champion)
-     }
-     const baseMana = champion.stats.mp
-     const growthMana = champion.stats.mpperlevel
-     const averageMana = parseInt(baseMana / growthMana)
-     if (averageMana === 10) {
-        topMana.push(champion)
-     }
-  });
-  if (stats == 'attackdamage') {
-     return topDamage
-  } else if (stats == 'hpregen') {
-     return topHealth
-  } else if (stats == 'mana') {
-     return topMana
-  }
+  let sumDamage= arrayDamage.reduce((item1, item2) => item1 + item2 , 0);
+  let averageDamage= sumDamage/dataLOL.length;
+  let averageRounded= Math.round((averageDamage)* 100) / 100;
+  return averageRounded;
+//console.log(averageRounded);
 }
+
+
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,24 +1,103 @@
-
+import {sortChampions, statistics1, statistics2} from "./data.js";
+import {filterByKey} from "./data.js";
 import data from './data/lol/lol.js';
-const mostrarInfos = Object.values(data.data);
+let dataLOL = Object.values(data.data);
 
-function printarCards(data) {
-    const mostrarCards = document.getElementById("container-dos-cards");
-    mostrarCards.innerHTML = data
-        .map((item) =>
-            `<div class ="print-cards">
-            <img class="imagem-campeão" src="${item.splash}">  
-            <p> : ${item.title}</p>
-            
-        </div>`
+document.getElementById("btnChampion").addEventListener("click", hidepages)
 
-        ).join("")
+function hidepages() {
+  document.getElementById("pag1").style.display = "none";
+  document.getElementById("pag2").style.display = "block";
+  document.getElementById("pag3").style.display = "none";
+}
+
+document.getElementById("goals").addEventListener("click", showPag3)
+
+function showPag3() {
+  document.getElementById("pag1").style.display = "none";
+  document.getElementById("pag2").style.display = "none";
+  document.getElementById("pag3").style.display = "block";
 }
 
 
-printarCards(mostrarInfos);
-const statusFilter = document.getElementById("status-filter")
-statusFilter.addEventListener("change",(event) => {
-
-
+document.getElementById("return").addEventListener("click", () =>{
+  document.getElementById("pag1").style.display = "block";
+  document.getElementById("pag2").style.display = "none";
+  document.getElementById("pag3").style.display = "none";
 })
+
+//Mostrar a pagina los filtros e  ordem
+
+let bringForm = document.getElementById("cardPrincipal");
+
+const showInScreen = (showChampion) => {
+  bringForm.innerHTML = "";
+  showChampion.forEach((champion) => {
+    const image = champion.splash;
+    let card = document.createElement("div");
+    card.setAttribute("class", "cardChampion face");
+    card.innerHTML = `
+    <div class="flip-card">
+      <div class="cardContainer">
+        <div class="cardFront">
+          <h3>${champion.name}</h3>
+          <img class = "image" src = ${image}>
+          <h4>${champion.title}</h4>
+        </div>
+        <div class="cardBack">
+          <h3>Description</h3>
+          <h4>${champion.blurb}</h4>
+          <h3>Level of difficulty</h3>
+          <h4>${champion.info.difficulty}</h4>
+        </div>
+      </div>
+    </div>
+    `
+    bringForm.appendChild(card);
+  })
+};
+
+document.getElementById("btnAll").addEventListener("click", () => {
+  showInScreen(dataLOL);
+});
+
+document.getElementById("btnKiller").addEventListener("click", () => {
+  showInScreen(filterByKey(dataLOL, "Assassin"));
+})//Filtro por asesinos
+
+document.getElementById("btnMagic").addEventListener("click", () => {
+  showInScreen(filterByKey(dataLOL, "Mage"));
+})//Filtro por Magos
+
+document.getElementById("btnFighter").addEventListener("click", () => {
+  showInScreen(filterByKey(dataLOL, "Fighter"));
+})//Filtro por Luchadores
+
+document.getElementById("btnMarksman").addEventListener("click", () => {
+  showInScreen(filterByKey(dataLOL, "Marksman"));
+})//Filtro por Tiradores
+
+document.getElementById("btnTank").addEventListener("click", () => {
+  showInScreen(filterByKey(dataLOL, "Tank"));
+})//Filtro por Tanques
+
+document.getElementById("btnSupport").addEventListener("click", () => {
+  showInScreen(filterByKey(dataLOL, "Support"));
+})//Filtro por apoyos 
+
+const bringOrder = document.getElementById("order")
+bringOrder.addEventListener("change", () => {
+  let orderChampions = sortChampions(dataLOL)
+  if(bringOrder.value === "btnZA"){
+    showInScreen(orderChampions);
+  } else {
+    showInScreen(orderChampions.reverse());
+  }
+})
+
+document.getElementById("showstatistic").innerHTML="Sabias que los campeones de League of Legends tienen un promedio de ataque de " + statistics1(dataLOL) + " y ademas su promedio de daño en ataque es del " + statistics2(dataLOL)
+
+
+statistics1(dataLOL);
+statistics2(dataLOL);
+//console.log(arrayAttack);
