016095b17b65b39bf44b7b06b93048dc8f3edc5d
Julia Benedicto <juliabenedictohfvcjd@gmail.com>
2022-04-05 21:38:50 -0300

    Apagando arquivos e adicionando tela inicial

---
+++ src/champions.js
@@ -0,0 +1,38 @@
+export default {
+    Aatrox: {
+        name: "Aatrox",
+        img:
+            "https://www.masterypoints.com/assets/img/lol/champion_icons/Aatrox.png",
+        attack: 8,
+        defense: 4,
+        magic: 3,
+        difficulty: 4
+    },
+    Ahri: {
+        name: "Ahri",
+        img:
+            "https://www.masterypoints.com/assets/img/lol/champion_icons/Ahri.png",
+        attack: 3,
+        defense: 4,
+        magic: 8,
+        difficulty: 5
+    },
+    Akali: {
+        name: "Akali",
+        img:
+            "https://www.masterypoints.com/assets/img/lol/champion_icons/Akali.png",
+        attack: 5,
+        defense: 3,
+        magic: 8,
+        difficulty: 7
+    },
+    Alistar: {
+        name: "Alistar",
+        img:
+            "https://www.masterypoints.com/assets/img/lol/champion_icons/Alistar.png",
+        attack: 6,
+        defense: 9,
+        magic: 5,
+        difficulty: 7
+    }
+};
--- src/main.js
+++ src/main.js
@@ -1,117 +1,27 @@
-import {sortChampions, statistics1, statistics2} from "./data.js";
-import {filterByKey} from "./data.js";
-import data from './data/lol/lol.js';
-let dataLOL = Object.values(data.data);
+import champions from './champions.js';
 
-document.getElementById("btnChampion").addEventListener("click", hidepages)
+//const championsList = champions.map(addName => addName.name.map);
 
-function hidepages() {
-  document.getElementById("pag1").style.display = "none";
-  document.getElementById("pag2").style.display = "block";
-  document.getElementById("pag3").style.display = "none";
-}
+//const sortChampions = document.querySelectorById('#sortChampions');
 
-document.getElementById("goals").addEventListener("click", showPag3)
-
-function showPag3() {
-  document.getElementById("pag1").style.display = "none";
-  document.getElementById("pag2").style.display = "none";
-  document.getElementById("pag3").style.display = "block";
-}
-
-
-document.getElementById("return").addEventListener("click", () =>{
-  document.getElementById("pag1").style.display = "block";
-  document.getElementById("pag2").style.display = "none";
-  document.getElementById("pag3").style.display = "none";
-})
-
-//Mostrar  na pagina os filtros e orden 
-
-let bringForm = document.getElementById("cardPrincipal");
-
-const showInScreen = (showChampion) => {
-  bringForm.innerHTML = "";
-  showChampion.forEach((champion) => {
-    const image = champion.splash;
-    let card = document.createElement("div");
-    card.setAttribute("class", "cardChampion face");
-    // CRIANDO COM LITERAL TEMPLATE
-    card.innerHTML = `
-    <div class="flip-card">
-      <div class="cardContainer">
-        <div class="cardFront">
-          <h3>${champion.name}</h3>
-          <img class = "image" src = ${image}>
-          <h4>${champion.title}</h4>
-        </div>
-        <div class="cardBack">
-          <h3>Description</h3>
-          <h4>${champion.blurb}</h4>
-          <h3>Level of difficulty</h3>
-          <h4>${champion.info.difficulty}</h4>
-        </div>
-      </div>
-    </div>
-    `
-    bringForm.appendChild(card);
-  })
-};
-
-document.getElementById("btnAll").addEventListener("click", () => {
-  showInScreen(dataLOL);
+sortChampions.addEventListener('click', () => {
+  showCards();
 });
 
-document.getElementById("btnKiller").addEventListener("click", () => {
-  showInScreen(filterByKey(dataLOL, "Assassin"));
-})//Filtro por assassinos
-
-document.getElementById("btnMagic").addEventListener("click", () => {
-  showInScreen(filterByKey(dataLOL, "Mage"));
-})//Filtro por Magos
-
-document.getElementById("btnFighter").addEventListener("click", () => {
-  showInScreen(filterByKey(dataLOL, "Fighter"));
-})//Filtro por Lutadores
-
-document.getElementById("btnMarksman").addEventListener("click", () => {
-  showInScreen(filterByKey(dataLOL, "Marksman"));
-})//Filtro por aTiradores
-
-document.getElementById("btnTank").addEventListener("click", () => {
-  showInScreen(filterByKey(dataLOL, "Tank"));
-})//Filtro por Tanques
-
-document.getElementById("btnSupport").addEventListener("click", () => {
-  showInScreen(filterByKey(dataLOL, "Support"));
-})//Filtro por apoios 
-
-const bringOrder = document.getElementById("order")
-bringOrder.addEventListener("change", () => {
-  let orderChampions = sortChampions(dataLOL)
-  if(bringOrder.value === "btnZA"){
-    showInScreen(orderChampions);
-  } else {
-    showInScreen(orderChampions.reverse());
-  }
-})
-
-//const bringAverage= document.getElementById("average");
-
-// const showAverage= () =>{
-//   const div= document.createElement("div");
-//   div.setAttribute("class", "curiousFact");
-//   div.innerHTML= `
-//   <h3> Dato Curioso </h3>
-//   <p> Sabias que los campeones de League of Legends tienen un promedio de ataque de ${statistics1(dataLOL)} y ademas su promedio de daño en ataque es del ${statistics2}</p>
-//   `
-
-//   bringAverage.appendChild(div);
-// }
-
-document.getElementById("showstatistic").innerHTML="Você sabia que os campeões de League of Legends têm uma média de ataque de 5,5 e também seu dano médio de ataque é de 55,9" + statistics1(dataLOL) + " e também seu dano médio de ataque é " + statistics2(dataLOL)
-
-
-statistics1(dataLOL);
-statistics2(dataLOL);
-//console.log(arrayAttack);
+function showCards(champions) {
+  document.getElementById("cards").innerHTML = champions
+    .map(
+      (item) =>
+        `
+           <img class="image-card" src="${item.img}" alt="Imagem dos  campeões">
+            <div class="name text-card">${item.name}</div>
+            <ul class="text-card">
+              <li>Ataque:${item.attack}</li>
+              <li>Defesa:${item.defense}</li>
+              <li>Magia:${item.magic}</li>
+            </ul>
+            `
+    )
+    .join("");
+}
+showCards(champions);
