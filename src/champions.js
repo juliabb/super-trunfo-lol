c86e0e13ba0ebe78854d2198180e20404b23f09f
Taila Martins <tailamartins74@gmail.com>
2022-04-06 19:56:38 -0300

    fetch: funcionalidade trunfo

--- src/champions.js
+++ src/champions.js
@@ -1,38 +1,51 @@
-export default [
-    {
-        name: "Aatrox",
-        img:
-            "https://img.redbull.com/images/q_auto,f_auto/redbullcom/2019/03/22/6ca582a8-d1e8-4972-b21e-6c405c4fc1f9/teemo-lol-omega-skin",
-        attack: 8,
-        defense: 4,
-        magic: 3,
-        difficulty: 4
-    },
-    {
-        name: "Ahri",
-        img:
-            "https://img.redbull.com/images/q_auto,f_auto/redbullcom/2019/03/22/6ca582a8-d1e8-4972-b21e-6c405c4fc1f9/teemo-lol-omega-skin",
-        attack: 3,
-        defense: 4,
-        magic: 8,
-        difficulty: 5
-    },
-    {
-        name: "Akali",
-        img:
-            "https://img.redbull.com/images/q_auto,f_auto/redbullcom/2019/03/22/6ca582a8-d1e8-4972-b21e-6c405c4fc1f9/teemo-lol-omega-skin",
-        attack: 5,
-        defense: 3,
-        magic: 8,
-        difficulty: 7
-    },
-    {
-        name: "Alistar",
-        img:
-            "https://img.redbull.com/images/q_auto,f_auto/redbullcom/2019/03/22/6ca582a8-d1e8-4972-b21e-6c405c4fc1f9/teemo-lol-omega-skin",
-        attack: 6,
-        defense: 9,
-        magic: 5,
-        difficulty: 7
-    }
-]
+
+export let carta1 = {
+  name: "Aatrox",
+  img:
+  "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg",
+  atributes:{
+    attack: 8,
+    defense: 4,
+    magic: 3,
+
+  }
+};
+
+export  let carta2 = {
+  name: "Ahri",
+  img:
+  "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg",
+  atributes: {
+    attack: 3,
+    defense: 4,
+    magic: 8,
+
+  }
+};
+
+export let carta3 = {
+  name: "Akali",
+  img:
+  "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Akali_0.jpg",
+  atributes:{
+    attack: 5,
+    defense: 3,
+    magic: 8,
+
+  }
+};
+
+export let carta4 = {
+  name: "Alistar",
+  img:
+  "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Alistar_0.jpg",
+  atributes:{
+    attack: 6,
+    defense: 9,
+    magic: 5,
+
+}
+}
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,26 +1,99 @@
-import champions from './champions.js';
+import {carta1, carta2, carta3, carta4} from "./champions.js"
 
-//const championsList = champions.map(addName => addName.name.map);
+let cartas = [carta1, carta2, carta3, carta4];
 
-const sortChampions = document.querySelector('#sortChampions');
+let cartaMaquina;
+let cartaJogador;
 
-sortChampions.addEventListener('click', () => {
-  showCards([]);
-});
 
-function showCards(champions) {
-  document.getElementById("cards").innerHTML = champions.map(
-      (item) =>
-        `
-           <img class="image-card" src="${item.img}" alt="Imagem dos  campeões">
-            <div class="name text-card">${item.name}</div>
-            <ul class="text-card">
-              <li>Ataque:${item.attack}</li>
-              <li>Defesa:${item.defense}</li>
-              <li>Magia:${item.magic}</li>
-            </ul>
-            `
-    )
-    .join("");
+
+function sortearCarta(){
+  let numeroCartaMaquina = parseInt(Math.random()*4);
+  cartaMaquina = cartas[numeroCartaMaquina];
+  // console.log(cartaMaquina);
+
+  let numeroCartaJogador = parseInt(Math.random()*4);
+  while(numeroCartaMaquina == numeroCartaJogador){
+    numeroCartaJogador = parseInt(Math.random()*4);
+  }
+
+  cartaJogador = cartas[numeroCartaJogador];
+
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+
+  // exibirOpcoes();
+  exibirCartaJogador();
 }
-showCards(champions);
+
+document.getElementById("btnSortear").addEventListener("click", sortearCarta)
+document.getElementById("btnJogar").addEventListener("click", jogar)
+
+
+function obtemAtributoSelecionado(){
+  let radioAtributos = document.getElementsByName("atributo");
+
+  for( let i = 0; i  < radioAtributos.length; i++){
+    if (radioAtributos[i].checked==true){
+      return radioAtributos[i].value;
+    }
+  }
+}
+let htmlResultado = document.getElementById("resultado")
+function jogar(){
+  let atributoSelecionado = obtemAtributoSelecionado();
+  let divResultado = document.getElementById("resultado");
+
+  if(cartaJogador.atributes[atributoSelecionado] > cartaMaquina.atributes[atributoSelecionado]){
+
+    htmlResultado = "<p class='resultado-final'>Venceu</p>"
+  }else if(cartaMaquina.atributes[atributoSelecionado] > cartaJogador.atributes[atributoSelecionado]){
+
+    htmlResultado = "<p class='resultado-final'>Perdeu</p>"
+  }else{
+
+    htmlResultado = "<p class='resultado-final'>Empatou</p>"
+  }
+  divResultado.innerHTML = htmlResultado;
+  document.getElementById('btnJogar').disabled = true;
+  exibirCartaMaquina();
+}
+
+function exibirCartaJogador(){
+  let divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage=`url(${cartaJogador.img})`;
+
+
+  let moldura ='<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  let tagHTML = "<div id='opcoes' class='carta-status'>"
+
+  let opcoesTexto = "";
+  for(let atributo in cartaJogador.atributes){
+    opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributes[atributo] + "<br>";
+
+  }
+
+  const nome = `<p class="carta-subtitle">${cartaJogador.name}</p>`
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina(){
+  let divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage=`url(${cartaMaquina.img})`;
+
+
+  const moldura ='<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  const tagHTML = "<div id='opcoes' class='carta-status'>"
+
+  let opcoesTexto = "";
+  for(let atributo in cartaMaquina.atributes){
+    opcoesTexto += "<p name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributes[atributo] + "</p>";
+
+  }
+
+  const nome = `<p class="carta-subtitle">${cartaMaquina.name}</p>`
+
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
