c268e119f94f26f5c50d542007459a415b2e234e
Julia Benedicto <juliabenedictohfvcjd@gmail.com>
2022-04-07 16:35:21 -0300

    Updates

---
+++ champions.js
@@ -0,0 +1,47 @@
+export const carta1 = {
+  name: "Aatrox",
+  img:
+    "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg",
+  atributes: {
+    attack: 8,
+    defense: 4,
+    magic: 3,
+
+  }
+};
+
+export const carta2 = {
+  name: "Ahri",
+  img:
+    "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg",
+  atributes: {
+    attack: 3,
+    defense: 4,
+    magic: 8,
+
+  }
+};
+
+export const carta3 = {
+  name: "Akali",
+  img:
+    "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Akali_0.jpg",
+  atributes: {
+    attack: 5,
+    defense: 3,
+    magic: 8,
+
+  }
+};
+
+export const carta4 = {
+  name: "Alistar",
+  img:
+    "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Alistar_0.jpg",
+  atributes: {
+    attack: 6,
+    defense: 9,
+    magic: 5,
+
+  }
+}
